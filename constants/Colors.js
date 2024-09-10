dfa55ea2817366f5d23bceda2948eb8ea2031e3c
codefactor-io <support@codefactor.io>
2024-09-10 06:39:19 +0000

    [CodeFactor] Apply fixes

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,10 @@
 export const colors = {
-  blue: '#30558C',
-  lightBlue: '#E4F3F9',
-  darkPink: '#F193B9',
-  superDarkPink: '#F5458C',
-  lightPink: '#F7A1C3',
-  lightBlueMenu: '#B6E1F3',
-  lightBlueBtmSheet: '#B9E7fA',
-  textBtmSheet: '#30558C',
+  blue: "#30558C",
+  lightBlue: "#E4F3F9",
+  darkPink: "#F193B9",
+  superDarkPink: "#F5458C",
+  lightPink: "#F7A1C3",
+  lightBlueMenu: "#B6E1F3",
+  lightBlueBtmSheet: "#B9E7fA",
+  textBtmSheet: "#30558C",
 };
